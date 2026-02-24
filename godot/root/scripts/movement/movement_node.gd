95cdcb6d7f2804e3c55ecfcc084de2787004cb23
parmeisan <parmeisan@gmail.com>
2026-02-24 16:21:55 -0600

    Roof is almost working

--- godot/root/scripts/movement/movement_node.gd
+++ godot/root/scripts/movement/movement_node.gd
@@ -1,4 +1,10 @@
-extends Node
+extends Node2D
 class_name movement_node
 
-@export var motion_obj : Node
+@export var motion_obj : PhysicsBody2D
+
+var velocity : Vector2 :
+	get:
+		return motion_obj.velocity
+	set (value):
+		motion_obj.velocity = value
--- godot/root/scripts/movement/wasd_mover.gd
+++ godot/root/scripts/movement/wasd_mover.gd
@@ -8,10 +8,8 @@ const ACCEL = 20.0
 func _physics_process(delta: float) -> void:
 	
 	 # Inputs
-	var sprinting = Input.is_action_pressed("move_sprint")
+	var sprinting : bool = Input.is_action_pressed("move_sprint")
 	var direction := Input.get_vector("move_left", "move_right","move_up", "move_down")
-	
-	var velocity = motion_obj.velocity
 	
 	# Main motion
 	if !sprinting:
@@ -19,19 +17,4 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = lerp(velocity, direction * HIGHSPEED, ACCEL * delta)
 	
-	# Visuals
-	#if abs(velocity.x) > abs (velocity.y):
-		#if velocity.x > 0: 
-			#get_node("Right").visible = true
-		#elif velocity.x < 0:
-			#get_node("Left").visible = true
-	#elif abs(velocity.y) > abs (velocity.x):
-		#if velocity.y > 0:
-			#get_node("Facing").visible = true
-		#elif velocity.y < 0:
-			#get_node("Away").visible = true
-	#else:
-		#get_node("Facing").visible = true
-	
-	motion_obj.velocity = velocity
 	motion_obj.move_and_slide()

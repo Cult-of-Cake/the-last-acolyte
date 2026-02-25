6e22142ebca36fc241cd13aef2cd5f3e2675daaa
parmeisan <parmeisan@gmail.com>
2026-02-25 17:59:22 -0600

    More linting fixes

--- godot/root/scripts/movement/PlayerControlled.gd
+++ godot/root/scripts/movement/PlayerControlled.gd
@@ -6,15 +6,15 @@ const HIGHSPEED = 300.0
 const ACCEL = 20.0
 
 func _physics_process(delta: float) -> void:
-	
-	 # Inputs
+
+	# Inputs
 	var sprinting : bool = Input.is_action_pressed("move_sprint")
 	var direction := Input.get_vector("move_left", "move_right","move_up", "move_down")
-	
+
 	# Main motion
 	if !sprinting:
 		velocity = lerp(velocity, direction * SPEED, ACCEL * delta)
 	else:
 		velocity = lerp(velocity, direction * HIGHSPEED, ACCEL * delta)
-	
+
 	move_and_slide()
--- godot/root/scripts/movement/Wanderer.gd
+++ godot/root/scripts/movement/Wanderer.gd
@@ -8,14 +8,15 @@ const DEBUG = false
 var _origin : Vector2
 var _goal : Vector2 = NO_GOAL
 var _waiting : bool = false
-var _heading : float			# Used to determine if we've reached our goal - ie if our heading has radically changed
+var _heading : float			# Used to determine if we've reached our goal
+								# ie if our heading has radically changed
 var delay : Timer
 
 @export var leash : Vector2 = Vector2(100, 100)
 
 func _ready() -> void:
 	_origin = posn
-	
+
 	# And set up a Timer.  We use this to pause after every movement.
 	delay = Timer.new()
 	delay.timeout.connect(get_new_goal)
@@ -24,16 +25,16 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
 	if _waiting: return
-	
+
 	# A radical shift in heading means we bypassed or nearly bypassed our goal
 	# And for the rare occasion that we got our target near-exact, check distance too
-	var _curr_heading : float = posn.angle_to_point(_goal)
+	var curr_heading : float = posn.angle_to_point(_goal)
 	#if DEBUG: print(_curr_heading, " originally ", _heading, " -> ", abs(_curr_heading - _heading))
-	if abs(_curr_heading - _heading) > 0.1 or posn.distance_to(_goal) < 3:
+	if abs(curr_heading - _heading) > 0.1 or posn.distance_to(_goal) < 3:
 		# Wait a moment, then find a new goal!
 		begin_waiting()
 		return
-	
+
 	# Main motion
 	posn = posn.move_toward(_goal, SPEED * delta)
 	move_and_slide()
@@ -43,17 +44,17 @@ func begin_waiting() -> void:
 		_waiting = true
 		delay.wait_time = randf_range(1, 7)
 		delay.start()
-	
+
 func get_new_goal() -> void:
 	delay.stop()
 	_goal = _origin + Vector2(randf_range(-leash.x, leash.x), randf_range(-leash.y, leash.y))
 	_heading = posn.angle_to_point(_goal)
-	
+
 	if DEBUG:
 		print("Done waiting. Goal position is now ", _goal)
 		queue_redraw()
 	_waiting = false
-	
+
 func _draw() -> void:
 	if DEBUG:
 		var rect : Rect2 = Rect2(to_local(_origin) - leash, leash * 2)

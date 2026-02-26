3557c3437e8b59ae0dffe0a1db1ba7a1b2d0485a
greenbas <steven.greenbank9+github2@gmail.com>
2026-02-26 16:39:31 -0600

    add dialogue entries

--- godot/root/scripts/creatures/npcs/Talkable.gd
+++ godot/root/scripts/creatures/npcs/Talkable.gd
@@ -2,5 +2,10 @@
 extends Node2D
 class_name Talkable
 
+@export var dialogue_file : DialogueResource
+@export var dialogue_balloon : PackedScene
+@export var starting_label : String = "start"
+
 func talk() -> void:
 	print ("You talked to ", get_parent().name)
+	DialogueManager.show_dialogue_balloon_scene(dialogue_balloon,dialogue_file,starting_label)

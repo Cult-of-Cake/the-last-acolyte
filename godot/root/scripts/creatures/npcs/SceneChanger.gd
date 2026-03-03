fe45a088b8ceefce92744656cb892dac1f51105f
parmeisan <parmeisan@gmail.com>
2026-03-02 19:56:17 -0600

    Linting fix

--- godot/root/scripts/creatures/npcs/SceneChanger.gd
+++ godot/root/scripts/creatures/npcs/SceneChanger.gd
@@ -7,7 +7,7 @@ func talk() -> void:
 	print ("You interacted with ", get_parent().name)
 	if new_scene != null:
 		get_tree().change_scene_to_packed(new_scene)
-		
+
 		# This transition looks nice, but I'm doing something wrong because then it crashes:
 		#var scene_manager_options: SceneManagerOptions = ResourceReference.get_scene_manager_options(
 			#"fade_play"

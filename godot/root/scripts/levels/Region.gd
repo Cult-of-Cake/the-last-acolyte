1f57e8bfcf7ad305bbc50c589f6d46aa6223177d
parmeisan <parmeisan@gmail.com>
2026-03-03 17:19:15 -0600

    Made into subscenes again

--- godot/root/scripts/levels/Region.gd
+++ godot/root/scripts/levels/Region.gd
@@ -1,13 +1,12 @@
 extends Area2D
 class_name Region
 
-@export var collision_shape : RectangleShape2D
 @export var collider : CollisionShape2D
 @export var match_area : MarginContainer
 
 func _ready() -> void:
+	var collision_shape : RectangleShape2D = RectangleShape2D.new()
 	var rect : Rect2 = match_area.get_rect()
 	position = (rect.size / 2)
-	collision_shape = RectangleShape2D.new()
 	collision_shape.size = rect.size
 	collider.shape = collision_shape

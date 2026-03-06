fb5dc577687e14c2c4d0cb83ca20deea8382b48d
greenbas <steven.greenbank9+github2@gmail.com>
2026-03-06 13:46:00 -0600

    create a character scene db

---
+++ godot/root/scenes/component/CharacterDialogueStyle.gd
@@ -0,0 +1,9 @@
+extends Resource 
+class_name CharacterDialogueStyle
+
+@export var portrait_dict : Dictionary[String,Texture2D]
+@export var bubble_theme : Theme = load("uid://32sqcntcr83f")
+@export var display_name_override : String 
+
+
+		

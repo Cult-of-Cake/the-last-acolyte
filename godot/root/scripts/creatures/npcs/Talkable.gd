6be83c1d4a5800ba457dd68a022afaddb73fcfbc
greenbas <steven.greenbank9+github2@gmail.com>
2026-03-07 14:52:10 -0600

    make dialogue not spawn more than once

--- godot/root/scripts/creatures/npcs/Talkable.gd
+++ godot/root/scripts/creatures/npcs/Talkable.gd
@@ -6,6 +6,17 @@ class_name Talkable
 @export var dialogue_balloon : PackedScene
 @export var starting_label : String = "start"
 
+func _ready() -> void:
+	DialogueManager.dialogue_ended.connect(_on_dialogue_ended)
+
 func talk() -> void:
+	if Vars.DIALOGUE_TAKEN:
+		LogWrapper.error(self,"Cannot Talk to character when dialogue already running!")
+		return
+	
+	Vars.DIALOGUE_TAKEN = true
 	print ("You talked to ", get_parent().name)
 	DialogueManager.show_dialogue_balloon_scene(dialogue_balloon,dialogue_file,starting_label)
+
+func _on_dialogue_ended(resource) -> void: 
+	Vars.DIALOGUE_TAKEN = false

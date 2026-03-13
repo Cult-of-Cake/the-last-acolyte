5da1495b29256af4793098962baf6ed5b9fed77a
parmeisan <parmeisan@gmail.com>
2026-03-12 18:27:16 -0600

    Intro camera pans

--- godot/root/scripts/ui/cutscenes/Intro.gd
+++ godot/root/scripts/ui/cutscenes/Intro.gd
@@ -2,10 +2,14 @@ extends CutsceneBase
 class_name Intro
 
 @export var player : Player
+@export var camera_rail_1 : PathFollow2D
 
 func run() -> void:
 	enter_cutscene_mode()
 	player.position = Vector2(37.0, 627.0)
-	
-	
+	var rail_tween := create_tween()
+	rail_tween.tween_property(camera_rail_1, "progress_ratio", 1.0, 8.0)
+	await rail_tween.finished
 	#exit_cutscene_mode()
+
+	

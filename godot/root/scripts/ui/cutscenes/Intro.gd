11c9685aeb9661cd67d28b255455df5d5db1c5f6
parmeisan <parmeisan@gmail.com>
2026-03-18 13:36:26 -0600

    Additional intro text, and (previous commit, mostly) jumping to dialogue labels

--- godot/root/scripts/ui/cutscenes/Intro.gd
+++ godot/root/scripts/ui/cutscenes/Intro.gd
@@ -10,19 +10,19 @@ func run() -> void:
 	enter_cutscene_mode()
 	player.position = Vector2(-1000.0, -1000.0)
 	
-	narrator.talk_from_label("start")
+	narrator.talk_from_label("intro_01")
 	await dialogue_done
 	
 	await tween_object(camera_rail_1, "progress_ratio", 1.0, 8.0)
 	
-	narrator.talk_from_label("continue_01")
+	narrator.talk_from_label("intro_02")
 	await dialogue_done
 
 	player.position = Vector2(222.0, 475.0)
 	tween_object(player, "position", Vector2(240.0, 410.0), 2.2)
 	await tween_object(cam, "zoom", Vector2(1.7, 1.7), 2.5)
 
-	god_tree.talk()
+	god_tree.talk_from_label("intro")
 	await dialogue_done
 	print("done")
 

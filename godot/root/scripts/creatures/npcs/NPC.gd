6c7f3f8fe534284d61de67925fe462c178a21628
parmeisan <parmeisan@gmail.com>
2026-03-19 08:24:15 -0600

    Put NPCs back after cutscene runs

--- godot/root/scripts/creatures/npcs/NPC.gd
+++ godot/root/scripts/creatures/npcs/NPC.gd
@@ -9,8 +9,13 @@ func _ready() -> void:
 		talkable = t
 	on_visibility_changed()
 
+func update_visibility(vis : bool) -> void:
+	visible = vis
+	on_visibility_changed()
+
 func on_visibility_changed() -> void:
 	if talkable:
+		print("setting talkable")
 		talkable.enabled = visible
 
 func debug_colour() -> Color:
